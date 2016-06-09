fa109d2539c36f142cef250989022c8b226330b1
Layne McNish <lmcnish14@gmail.com>
2016-06-09 09:44:22 -0700

    Subscription: Add attributes for initialization, convenience methods & validations

--- lib/bluebottle/models/subscription.rb
+++ lib/bluebottle/models/subscription.rb
@@ -1,1 +1,33 @@
-# implement this
+require 'active_support/all'
+
+module BlueBottle
+  module Models
+    class Subscription
+      attr_accessor :customer,
+                    :coffee,
+                    :status,
+                    :created_at
+
+      def initialize(customer, coffee, status)
+        @customer = customer
+        @coffee = coffee
+        @status = status ||= :active
+        @created_at = Time.now
+      end
+
+      def paused?
+        status == :paused
+      end
+
+      def cancelled?
+        status == :cancelled
+      end
+
+      def validate_cancellation(last_subscription)
+        if last_subscription && last_subscription.paused? && cancelled?
+          raise "You may not cancel a paused subscription."
+        end
+      end
+    end
+  end
+end
