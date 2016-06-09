7c9565256fe8bbedfcd19b68ebcd8392682c2a24
Layne McNish <lmcnish14@gmail.com>
2016-06-09 09:42:45 -0700

    Tests: Implement testing strategy for subscription_service

--- spec/bluebottle/coding_question_spec.rb
+++ spec/bluebottle/coding_question_spec.rb
@@ -1,5 +1,6 @@
 require 'bluebottle'
 require 'bluebottle/coding_question'
+require 'pry'
 
 describe BlueBottle::CodingQuestion do
   let(:sally) { BlueBottle::Models::Customer.new(1, 'Sally', 'Fields', 'sally@movies.com') }
@@ -27,44 +28,54 @@ describe BlueBottle::CodingQuestion do
 
   context 'Sally subscribes to Bella Donovan' do
     before do
-      # Establish subscription to Bella Donovan here
+      subscription_service.add_subscription(sally, bella_donovan)
     end
 
-    xit 'Sally should have one active subscription' do
+    it 'Sally should have one active subscription' do
+      expect(subscription_service.find_active_subscriptions_by_customer(sally).count).to eq(1)
     end
 
-    xit 'Bella Donovan should have one customer subscribed to it' do
+    it 'Bella Donovan should have one customer subscribed to it' do
+      expect(subscription_service.find_active_subscriptions_by_coffee(bella_donovan).count).to eq(1)
     end
   end
 
   context 'Liv and Elijah subscribe to Hayes Valley Espresso' do
     before do
-      # Establish subscriptions here
+      subscription_service.add_subscription(liv, hayes_valley_espresso)
+      subscription_service.add_subscription(elijah, hayes_valley_espresso)
     end
 
-    xit 'Liv should have one active subscription' do
+    it 'Liv should have one active subscription' do
+      expect(subscription_service.find_active_subscriptions_by_customer(liv).count).to eq(1)
     end
 
-    xit 'Elijah should have one active subscription' do
+    it 'Elijah should have one active subscription' do
+      expect(subscription_service.find_active_subscriptions_by_customer(elijah).count).to eq(1)
     end
 
-    xit 'Hayes Valley Espresso should have two customers subscribed to it' do
+    it 'Hayes Valley Espresso should have two customers subscribed to it' do
+      expect(subscription_service.find_active_subscriptions_by_coffee(hayes_valley_espresso).count).to eq(2)
     end
   end
 
   context 'Pausing:' do
     context 'when Liv pauses her subscription to Bella Donovan,' do
       before do
-        # Establish subscription here
+        subscription_service.add_subscription(liv, bella_donovan)
+        subscription_service.pause_subscription(liv, bella_donovan)
       end
 
-      xit 'Liv should have zero active subscriptions' do
+      it 'Liv should have zero active subscriptions' do
+        expect(subscription_service.find_active_subscriptions_by_customer(liv).count).to eq(0)
       end
 
-      xit 'Liv should have a paused subscription' do
+      it 'Liv should have a paused subscription' do
+        expect(subscription_service.find_customer_subscriptions_by_status(liv, :paused).count).to eq(1)
       end
 
-      xit 'Bella Donovan should have one customers subscribed to it' do
+      it 'Bella Donovan should have one customers subscribed to it' do
+        expect(subscription_service.find_active_subscriptions_by_coffee(bella_donovan).count).to eq(1)
       end
     end
   end
@@ -72,23 +83,27 @@ describe BlueBottle::CodingQuestion do
   context 'Cancelling:' do
     context 'when Jack cancels his subscription to Bella Donovan,' do
       before do
-        # Establish subscription here
+        subscription_service.add_subscription(jack, bella_donovan)
+        subscription_service.cancel_subscription(jack, bella_donovan)
       end
 
-      xit 'Jack should have zero active subscriptions' do
+      it 'Jack should have zero active subscriptions' do
+        expect(subscription_service.find_active_subscriptions_by_customer(jack).count).to eq(0)
       end
 
-      xit 'Bella Donovan should have zero active customers subscribed to it' do
+      it 'Bella Donovan should have zero active customers subscribed to it' do
+        expect(subscription_service.find_active_subscriptions_by_coffee(bella_donovan).count).to eq(0)
       end
 
       context 'when Jack resubscribes to Bella Donovan' do
         before do
-          # Establish subscription here
+          subscription_service.add_subscription(jack, bella_donovan)
         end
 
-        xit 'Bella Donovan has two subscriptions, one active, one cancelled' do
+        it 'Bella Donovan has two subscriptions, one active, one cancelled' do
+          expect(subscription_service.find_all_subscriptions_by_coffee(bella_donovan).count).to eql(2)
+          expect(subscription_service.find_all_subscriptions_by_coffee(bella_donovan).map(&:status)).to match_array([:active, :cancelled])
         end
-
       end
     end
   end
@@ -96,10 +111,12 @@ describe BlueBottle::CodingQuestion do
   context 'Cancelling while Paused:' do
     context 'when Jack tries to cancel his paused subscription to Bella Donovan,' do
       before do
-        # Establish paused subscription here
+        subscription_service.add_subscription(jack, bella_donovan)
+        subscription_service.pause_subscription(jack, bella_donovan)
       end
 
-      xit 'Jack raises an exception preventing him from cancelling a paused subscription' do
+      it 'Jack raises an exception preventing him from cancelling a paused subscription' do
+        expect{subscription_service.cancel_subscription(jack, bella_donovan)}.to raise_exception(RuntimeError, "You may not cancel a paused subscription.")
       end
     end
   end
